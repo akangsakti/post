579c8d82a2d854532400a90d90c1e57f1d73f7bb
akangsakti <github@disbox.org>
2019-04-15 16:02:23 +0700

    Update nyooxxx.js

--- nyooxxx.js
+++ nyooxxx.js
@@ -10,6 +10,6 @@ function disappeare3(){document.getElementById('question4').style.display="block
 function countDown(maxtime,fn)
 {var timer=setInterval(function()
 {if(maxtime>=0){minutes=Math.floor(maxtime/60);seconds=Math.floor(maxtime%60);if(seconds<10&&length.seconds!=2)seconds='0'+seconds;msg=+minutes+":"+seconds+" ";fn(msg);--maxtime;}
-else{clearInterval(timer);fn("00:00");}},1000);}
-countDown(60,function(msg)
+else{clearInterval(timer);fn("00:00");}},2000);}
+countDown(120,function(msg)
 {document.getElementById('timer1').innerHTML=msg;})
